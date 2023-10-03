68e3acbd587b7e3ddbece904dcff67674c4bb8bd
1Fenix7 <userpk66@gmail.com>
2023-10-03 20:50:50 +0300

    feat: :sparkles: add beauty notification when delete cabin

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { formatCurrency } from '../../utils/helpers';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { deleteCabins } from '../../services/apiCabins';
+import toast from 'react-hot-toast';
 
 const TableRow = styled.div`
 	display: grid;
@@ -50,12 +51,14 @@ function CabinRow({ cabin }) {
 	const { isLoading: isDeleting, mutate } = useMutation({
 		mutationFn: deleteCabins,
 		onSuccess: () => {
+			toast.success('Cabin succsessfully deleted');
+
 			queryClient.invalidateQueries({
 				queryKey: ['cabins'],
 			});
 		},
 		onError: (err) => {
-			console.error(err);
+			toast.error(err.message);
 		},
 	});
 
