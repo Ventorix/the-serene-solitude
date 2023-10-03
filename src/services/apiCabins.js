fe32d4c10fcae7d0480b46211c368ea4a9bbc9e6
1Fenix7 <userpk66@gmail.com>
2023-10-04 02:00:58 +0300

    feat(supabase): :sparkles: add update cabin query

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -22,12 +22,24 @@ export async function deleteCabin(id) {
 	return data;
 }
 
-export async function createCabin(newCabin) {
-	const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll('/', '');
-	const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+export async function createEditCabin(newCabin, id) {
+	const hasImagePath = newCabin.image?.startsWith?.(supabaseUrl);
 
-	//1. Create cabin
-	let { data, error } = await supabase.from('cabins').insert([{ ...newCabin, image: imagePath }]);
+	const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll('/', '');
+
+	const imagePath = hasImagePath
+		? newCabin.image
+		: `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+	let query = supabase.from('cabins');
+
+	// A) CREATE
+	if (!id) query = query.insert([{ ...newCabin, image: imagePath }]);
+
+	// B) EDIT
+	if (id) query = query.update({ ...newCabin, image: imagePath }).eq('id', id);
+
+	const { data, error } = await query.select().single();
 
 	if (error) {
 		console.error(error);
@@ -35,6 +47,8 @@ export async function createCabin(newCabin) {
 	}
 
 	// 2. Upload image
+	if (hasImagePath) return data;
+
 	const { error: storageError } = await supabase.storage
 		.from('cabin-images')
 		.upload(imageName, newCabin.image);
