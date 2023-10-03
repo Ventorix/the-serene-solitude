b9e91ae267e2a615f9c9624fc3704530ad514908
1Fenix7 <userpk66@gmail.com>
2023-10-03 03:29:19 +0300

    feat(supabase): :boom: add supabase in project

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://cdxnrefzaqylhufxlsbs.supabase.co';
+const supabaseKey =
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNkeG5yZWZ6YXF5bGh1Znhsc2JzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYyODkzOTUsImV4cCI6MjAxMTg2NTM5NX0.mGvg_iLiH2UFZAeBI0rY123Rn0QdvGU4Dxp4JcTHh1I';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
