78a3e9900ada53a308a52f057e415c755a446eb7
1Fenix7 <userpk66@gmail.com>
2023-10-04 21:28:59 +0300

    refactor: :recycle: refactor table.body component using render props pattern

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,42 +1,18 @@
 import { useCabins } from './useCabins';
 
-import styled from 'styled-components';
-
 import Spinner from '../../ui/Spinner';
 import CabinRow from './CabinRow';
 import Table from '../../ui/Table';
 
-// const Table = styled.div`
-// 	border: 1px solid var(--color-grey-200);
-
-// 	font-size: 1.4rem;
-// 	background-color: var(--color-grey-0);
-// 	border-radius: 7px;
-// 	overflow: hidden;
-// `;
-
-// const TableHeader = styled.header`
-// 	display: grid;
-// 	grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-// 	column-gap: 2.4rem;
-// 	align-items: center;
-
-// 	background-color: var(--color-grey-50);
-// 	border-bottom: 1px solid var(--color-grey-100);
-// 	text-transform: uppercase;
-// 	letter-spacing: 0.4px;
-// 	font-weight: 600;
-// 	color: var(--color-grey-600);
-// 	padding: 1.6rem 2.4rem;
-// `;
-
 function CabinTable() {
 	const { isLoading, cabins } = useCabins();
+
+	const defaultColumns = '1fr 1.8fr 2.2fr 1fr 1fr 1fr';
 
 	if (isLoading) return <Spinner />;
 
 	return (
-		<Table columns='1fr 1.8fr 2.2fr 1fr 1fr 1fr'>
+		<Table columns={defaultColumns}>
 			<Table.Header>
 				<div>Image</div>
 				<div>Cabin</div>
@@ -46,9 +22,7 @@ function CabinTable() {
 				<div>Actions</div>
 			</Table.Header>
 
-			{cabins.map((cabin) => (
-				<CabinRow cabin={cabin} key={cabin.id} />
-			))}
+			<Table.Body data={cabins} render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />} />
 		</Table>
 	);
 }
