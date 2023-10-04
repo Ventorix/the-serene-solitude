5009b34135b8200a93c7ab7a2a8c5568f1369bb9
1Fenix7 <userpk66@gmail.com>
2023-10-04 19:26:37 +0300

    refactor: :art: refactor modal component to compound component

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,26 +1,38 @@
-import { useState } from 'react';
 import Button from '../../ui/Button';
 import CreateCabinForm from './CreateCabinForm';
 import Modal from '../../ui/Modal';
 
 function AddCabin() {
-	const [isOpenModal, setIsOpenModal] = useState(false);
-
-	function handleOpen() {
-		setIsOpenModal((isOpen) => !isOpen);
-	}
-
 	return (
-		<>
-			<Button onClick={handleOpen}>Add new cabin</Button>
-
-			{isOpenModal && (
-				<Modal onCloseModal={handleOpen}>
-					<CreateCabinForm onCloseModal={handleOpen} />
-				</Modal>
-			)}
-		</>
+		<Modal>
+			<Modal.Open opens='cabin-form'>
+				<Button>Add new cabin</Button>
+			</Modal.Open>
+			<Modal.Window name='cabin-form'>
+				<CreateCabinForm />
+			</Modal.Window>
+		</Modal>
 	);
 }
+
+// function AddCabin() {
+// 	const [isOpenModal, setIsOpenModal] = useState(false);
+
+// 	function handleOpen() {
+// 		setIsOpenModal((isOpen) => !isOpen);
+// 	}
+
+// 	return (
+// 		<>
+// 			<Button onClick={handleOpen}>Add new cabin</Button>
+
+// 			{isOpenModal && (
+// 				<Modal onCloseModal={handleOpen}>
+// 					<CreateCabinForm onCloseModal={handleOpen} />
+// 				</Modal>
+// 			)}
+// 		</>
+// 	);
+// }
 
 export default AddCabin;
