b57306d7c1332475a5d7cb154c9495a60f6fd822
1Fenix7 <userpk66@gmail.com>
2023-10-07 01:15:12 +0300

    feat(supabase): :sparkles: add a feature to sort bookings data

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -8,8 +8,11 @@ export async function getBookings({ filter, sortBy }) {
 			'id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)',
 		);
 
-	// Filter
-	if (filter !== null) query = query[filter.method || 'eq'](filter.field, filter.value);
+	// 1. Filter
+	if (filter) query = query[filter.method || 'eq'](filter.field, filter.value);
+
+	// 2. Sort
+	if (sortBy) query = query.order(sortBy.field, { ascending: sortBy.direction === 'asc' });
 
 	const { data, error } = await query;
 
