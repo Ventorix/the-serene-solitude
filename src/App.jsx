ea73651bfbdc976036a1fd40d388279370990ee1
1Fenix7 <userpk66@gmail.com>
2023-10-07 03:31:43 +0300

    feat: :sparkles: add a feature to see a booking details by context menu

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import GlobalStyles from './styles/GlobalStyles';
 
+import AppLayout from './ui/AppLayout';
 import Dashboard from './pages/Dashboard';
 import Bookings from './pages/Bookings';
+import Booking from './pages/Booking';
 import Cabins from './pages/Cabins';
-import Users from './pages/Users';
 import Settings from './pages/Settings';
+import Users from './pages/Users';
 import Account from './pages/Account';
 import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
-import AppLayout from './ui/AppLayout';
+
 import { Toaster } from 'react-hot-toast';
 
 const queryClient = new QueryClient({
@@ -35,6 +37,7 @@ function App() {
 						<Route index element={<Navigate replace to='dashboard' />} />
 						<Route path='dashboard' element={<Dashboard />} />
 						<Route path='bookings' element={<Bookings />} />
+						<Route path='bookings/:bookingId' element={<Booking />} />
 						<Route path='cabins' element={<Cabins />} />
 						<Route path='users' element={<Users />} />
 						<Route path='settings' element={<Settings />} />
