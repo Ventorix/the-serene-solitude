f1ebb15c676028db81b3c99375b25020b6527a6c
1Fenix7 <userpk66@gmail.com>
2023-10-09 03:15:07 +0300

    feat: :sparkles: add a sign up form and validation for it

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -1,38 +1,87 @@
-import Button from "../../ui/Button";
-import Form from "../../ui/Form";
-import FormRow from "../../ui/FormRow";
-import Input from "../../ui/Input";
+import { useForm } from 'react-hook-form';
+import Button from '../../ui/Button';
+import Form from '../../ui/Form';
+import FormRow from '../../ui/FormRow';
+import Input from '../../ui/Input';
 
+// FullName regex: /^[a-zA-Z]+ [a-zA-Z]+$/
 // Email regex: /\S+@\S+\.\S+/
+// Password regex: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/
 
 function SignupForm() {
-  return (
-    <Form>
-      <FormRow label="Full name" error={""}>
-        <Input type="text" id="fullName" />
-      </FormRow>
+	const { register, formState, getValues, handleSubmit } = useForm();
+	const { errors } = formState;
 
-      <FormRow label="Email address" error={""}>
-        <Input type="email" id="email" />
-      </FormRow>
+	function onSubmit(data) {
+		console.log(data);
+	}
 
-      <FormRow label="Password (min 8 characters)" error={""}>
-        <Input type="password" id="password" />
-      </FormRow>
+	return (
+		<Form onSubmit={handleSubmit(onSubmit)}>
+			<FormRow label='Full name' error={errors?.fullName?.message}>
+				<Input
+					autoComplete='name'
+					type='text'
+					id='fullName'
+					{...register('fullName', {
+						required: 'This field is required',
+						pattern: {
+							value: /^[a-zA-Z]+ [a-zA-Z]+$/,
+							message: 'Please provide a valid full name',
+						},
+					})}
+				/>
+			</FormRow>
 
-      <FormRow label="Repeat password" error={""}>
-        <Input type="password" id="passwordConfirm" />
-      </FormRow>
+			<FormRow label='Email address' error={errors?.email?.message}>
+				<Input
+					autoComplete='email'
+					type='email'
+					id='email'
+					{...register('email', {
+						required: 'This field is required',
+						pattern: { value: /\S+@\S+\.\S+/, message: 'Please provide a valid email address' },
+					})}
+				/>
+			</FormRow>
 
-      <FormRow>
-        {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
-          Cancel
-        </Button>
-        <Button>Create new user</Button>
-      </FormRow>
-    </Form>
-  );
+			<FormRow label='Password (min 8 characters)' error={errors?.password?.message}>
+				<Input
+					autoComplete='new-password'
+					type='password'
+					id='password'
+					{...register('password', {
+						required: 'This field is required',
+						pattern: {
+							value: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/,
+							message:
+								'Please provide a valid password with at least one number, one lowercase and one uppercase letter',
+						},
+					})}
+				/>
+			</FormRow>
+
+			<FormRow label='Repeat password' error={errors?.passwordConfirm?.message}>
+				<Input
+					autoComplete='new-password'
+					type='password'
+					id='passwordConfirm'
+					{...register('passwordConfirm', {
+						required: 'This field is required',
+						validate: (value) => value === getValues().password || 'Passwords must to match',
+					})}
+				/>
+			</FormRow>
+
+			<FormRow>
+				{/* type is an HTML attribute! */}
+				<Button variation='secondary' type='reset'>
+					Cancel
+				</Button>
+				<Button>Create new user</Button>
+			</FormRow>
+		</Form>
+	);
 }
 
 export default SignupForm;
