ed84f184490e303d8ea3e30208706900504969c9
1Fenix7 <userpk66@gmail.com>
2023-10-10 05:14:55 +0300

    feat: :sparkles: add a pie chart graphic to the Dashboard page

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import { useResentStays } from './useRecentStays';
 import Stats from './Stats';
 import { useCabins } from '../cabins/useCabins';
 import SalesChart from './SalesChart';
+import DurationChart from './DurationChart';
 
 const StyledDashboardLayout = styled.div`
 	display: grid;
@@ -30,7 +31,7 @@ function DashboardLayout() {
 				cabinCount={cabins.length}
 			/>
 			<div>Today`s activity</div>
-			<div>Chart stay duration</div>
+			<DurationChart confirmedStays={confirmedStays} />
 			<SalesChart bookings={bookings} numDays={numDays} />
 		</StyledDashboardLayout>
 	);
