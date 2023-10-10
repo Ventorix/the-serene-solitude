7e7e154ac275cffba835a9b363ddc8c907fcc9ac
1Fenix7 <userpk66@gmail.com>
2023-10-10 03:22:50 +0300

    feat: :sparkles: add hooks for computation of bookings by time

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,8 @@
 import styled from 'styled-components';
+import Spinner from '../../ui/Spinner';
+
+import { useResentBookings } from './useRecentBookings';
+import { useResentStays } from './useRecentStays';
 
 const StyledDashboardLayout = styled.div`
 	display: grid;
@@ -8,10 +12,16 @@ const StyledDashboardLayout = styled.div`
 `;
 
 function DashboardLayout() {
+	const { isLoading, bookings } = useResentBookings();
+	const { isLoading: isStaing, stays, confirmedStays } = useResentStays();
+
+	if (isLoading || isStaing) return <Spinner />;
+
+	console.log(bookings);
 	return (
 		<StyledDashboardLayout>
 			<div>Statistics</div>
-			<div>Today's activity</div>
+			<div>Today`s activity</div>
 			<div>Chart stay duration</div>
 			<div>Chart of sales</div>
 		</StyledDashboardLayout>
