345f63eeb574dff1a59837a8a5990e95b7e538bb
1Fenix7 <userpk66@gmail.com>
2023-10-10 03:48:15 +0300

    feat: :sparkles: add app statistics to the Dashboard page

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -3,6 +3,8 @@ import Spinner from '../../ui/Spinner';
 
 import { useResentBookings } from './useRecentBookings';
 import { useResentStays } from './useRecentStays';
+import Stats from './Stats';
+import { useCabins } from '../cabins/useCabins';
 
 const StyledDashboardLayout = styled.div`
 	display: grid;
@@ -13,14 +15,20 @@ const StyledDashboardLayout = styled.div`
 
 function DashboardLayout() {
 	const { isLoading, bookings } = useResentBookings();
-	const { isLoading: isStaing, stays, confirmedStays } = useResentStays();
+	const { isLoading: isStaing, stays, confirmedStays, numDays } = useResentStays();
+	const { cabins, isLoading: isLoadingCabins } = useCabins();
 
-	if (isLoading || isStaing) return <Spinner />;
+	if (isLoading || isStaing || isLoadingCabins) return <Spinner />;
 
 	console.log(bookings);
 	return (
 		<StyledDashboardLayout>
-			<div>Statistics</div>
+			<Stats
+				bookings={bookings}
+				confirmedStays={confirmedStays}
+				numDays={numDays}
+				cabinCount={cabins.length}
+			/>
 			<div>Today`s activity</div>
 			<div>Chart stay duration</div>
 			<div>Chart of sales</div>
