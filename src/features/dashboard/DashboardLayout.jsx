423d35eb9a927c9e6770f32d74dece37e0d73b2d
1Fenix7 <userpk66@gmail.com>
2023-10-10 05:44:50 +0300

    feat: :sparkles: add a list of today activities to the Dashboard page

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -7,6 +7,7 @@ import Stats from './Stats';
 import { useCabins } from '../cabins/useCabins';
 import SalesChart from './SalesChart';
 import DurationChart from './DurationChart';
+import TodayActivity from '../check-in-out/TodayActivity';
 
 const StyledDashboardLayout = styled.div`
 	display: grid;
@@ -17,7 +18,7 @@ const StyledDashboardLayout = styled.div`
 
 function DashboardLayout() {
 	const { isLoading, bookings } = useResentBookings();
-	const { isLoading: isStaing, stays, confirmedStays, numDays } = useResentStays();
+	const { isLoading: isStaing, confirmedStays, numDays } = useResentStays();
 	const { cabins, isLoading: isLoadingCabins } = useCabins();
 
 	if (isLoading || isStaing || isLoadingCabins) return <Spinner />;
@@ -30,7 +31,7 @@ function DashboardLayout() {
 				numDays={numDays}
 				cabinCount={cabins.length}
 			/>
-			<div>Today`s activity</div>
+			<TodayActivity />
 			<DurationChart confirmedStays={confirmedStays} />
 			<SalesChart bookings={bookings} numDays={numDays} />
 		</StyledDashboardLayout>
